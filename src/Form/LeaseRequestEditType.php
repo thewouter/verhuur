2b4ac9e20b98a56533e9215a57bcc9db538fb49b
Wouter van Harten <wouter@woutervanharten.nl>
2019-03-25 19:05:47 +0100

    removed too much accidentally

--- src/Form/LeaseRequestEditType.php
+++ src/Form/LeaseRequestEditType.php
@@ -26,6 +26,7 @@ use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\Extension\Core\Type\MoneyType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\TimeType;
 
 /**
  * Defines the form used to create and manipulate blog posts.
@@ -69,6 +70,20 @@ class LeaseRequestEditType extends AbstractType {
                 'widget' => 'single_text',
                 'years' => array(date('Y'), date('Y') + 1),
                 'model_timezone' => 'Europe/Amsterdam',
+                'disabled' => true,
+            ])
+            ->add('key_deliver', TimeType::class, [
+                'label' => 'label.start_time',
+                'widget' => 'choice',
+                'hours' => range(9,22),
+                'minutes' => range(0,60,15),
+                'disabled' => true,
+            ])
+            ->add('key_return', TimeType::class, [
+                'label' => 'label.end_time',
+                'widget' => 'choice',
+                'hours' => range(9,22),
+                'minutes' => range(0,60,15),
                 'disabled' => true,
             ])
             ->add('num_attendants', IntegerType::class, [
